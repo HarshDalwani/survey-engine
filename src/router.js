52b03bf6c5848c5b6cb10e27e9131744be89e613
server-at-316666916931 <harshdalwani124@gmail.com>
2019-09-05 17:56:28 +0530

    router added

--- src/router.js
+++ src/router.js
@@ -1,24 +1,36 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
-import Auth from './components/Auth.vue'
-import AuthSuccess from './components/AuthSuccess.vue'
-import SignUp from './components/SignUp.vue'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+//User
+import Auth from "./components/user/Auth.vue";
+import AuthSuccess from "./components/user/AuthSuccess.vue";
+import SignUp from "./components/user/SignUp.vue";
+
+//Layout
+import Home from "./components/layout/Home.vue";
+import Dashboard from "./components/layout/Dashboard.vue";
+import Question from "./components/layout/Question.vue";
+import SideNav from "./components/layout/SideNav.vue";
+import Survey from "./components/layout/Survey.vue";
+import Reports from "./components/layout/Reports.vue";
+
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {path:'/auth' , component:Auth},
-    {path:'/success' , component:AuthSuccess},
-    {path:'/signup' , component:SignUp},
+    //user
+    { path: "/auth", component: Auth },
+    { path: "/success", component: AuthSuccess },
+    { path: "/signup", component: SignUp },
 
+    //layout
+    { path: "/", component: Home },
+    { path: "/dashboard", component: Dashboard },
+    { path: "/question", component: Question },
+    { path: "/sidenav", component: SideNav },
+    { path: "/survey", component: Survey },
+    { path: "/reports", component: Reports }
   ]
-})
+});
